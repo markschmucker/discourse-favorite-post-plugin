9d7cc1f1c71757db60e719d5b8faf888866659ca
markschmucker <36678240+markschmucker@users.noreply.github.com>
2019-10-09 13:07:34 +0930

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,18 @@ after_initialize {
   class ::Jobs::FavoritePost
     def execute(args)
       SiteSetting.favorite_post = 12345
+      
+      user = User.find_by_username('JohnDoe')
+      min_date = Time.now - (5 * 24 * 60 * 60)
+      
+      @popular_posts = Post
+          .order("posts.score DESC")
+          .for_mailing_list(user, min_date)
+          .where('posts.post_type = ?', Post.types[:regular])
+          .where('posts.deleted_at IS NULL AND posts.hidden = false AND posts.user_deleted = false')
+          .where("posts.post_number > ?", 1)
+          .where('posts.created_at < ?', (SiteSetting.editing_grace_period || 0).seconds.ago)
+          .limit(5)
     end
   end
 }
-
