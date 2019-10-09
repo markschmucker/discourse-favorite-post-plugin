6ede94238e97a504f5bcea92e629fe72c438a66a
markschmucker <36678240+markschmucker@users.noreply.github.com>
2019-10-09 12:57:45 +0930

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,14 @@
+# name: favorite-post
+# about: Find Favorite Post
+# authors: Mark Schmucker
+# version: 0.1
+
+after_initialize {
+  class ::Jobs::FavoritePost
+    def execute(args)
+      
+      
+    end
+  end
+}
 
