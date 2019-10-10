51b8a3b261c986ce193f31c695a7d13e6abe94f4
markschmucker <36678240+markschmucker@users.noreply.github.com>
2019-10-10 15:34:15 +0930

    select all posts with max likes if > 4

--- plugin.rb
+++ plugin.rb
@@ -9,16 +9,26 @@ after_initialize {
       SiteSetting.favorite_post = 12345
       
       user = User.find_by_username('JohnDoe')
-      min_date = Time.now - (5 * 24 * 60 * 60)
+      min_date = Time.now - (1 * 24 * 60 * 60)
       
-      favorite_posts = Post
-          .order("posts.score DESC")
+      posts = Post
+          .order("posts.like_count DESC")
           .for_mailing_list(user, min_date)
           .where('posts.post_type = ?', Post.types[:regular])
           .where('posts.deleted_at IS NULL AND posts.hidden = false AND posts.user_deleted = false')
           .where("posts.post_number > ?", 1)
           .where('posts.created_at < ?', (SiteSetting.editing_grace_period || 0).seconds.ago)
-          .limit(1)
+          .limit(10)
+      
+      max_like_count = posts.map(|post| post.like_count).max
+      
+      favorite_posts = nil
+      if max_like_count > 4
+        favorite_posts = posts.select(|post| post.like_count == max_like_count)
+      end
+      
+      # todo: join them as a string? or easier, move this to the digest
+      # plugin; no need to run separately.
       favorite_posts.each do |post|
         puts post
         SiteSetting.favorite_post = post.id
